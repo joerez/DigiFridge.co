b5bdfbf37499b629c4ab9cc4e24812e66d95bc3d
Joe Rezendes <rezenbiz@gmail.com>
2018-01-07 19:26:43 -0800

    remade it as a fridge

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,6 +4,17 @@ $(document).ready(() => {
       $( "#sortable" ).sortable();
     } );
 
+    $( function() {
+      $( ".draggable" ).draggable({
+        // When the user starts dragging, make sure the z-index for this
+   // helper is higher than all other draggables.
+   start: function ( e, ui ) {
+       $( ".ui-draggable" ).not( ui.helper.css( "z-index", "1" ) )
+                           .css( "z-index", "0" );
+   }
+      });
+    });
+
     $("#reveal").on("click", function() {
       $("#come").removeClass('hidden').addClass('animated fadeInDown');
     });
